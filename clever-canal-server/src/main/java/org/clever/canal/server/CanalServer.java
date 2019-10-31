f930541be687480b2fbe6bad9e7586d02f7d793a
lizw <lzw1000000@163.com>
2019-10-31 14:31:57 +0800

    调整项目结构 优化依赖

--- clever-canal-server/src/main/java/org/clever/canal/server/CanalServer.java
+++ clever-canal-server/src/main/java/org/clever/canal/server/CanalServer.java
@@ -7,7 +7,6 @@ import org.clever.canal.server.exception.CanalServerException;
 /**
  * 对应canal整个服务实例，一个jvm实例只有一份server
  */
-@SuppressWarnings("unused")
 public interface CanalServer extends CanalLifeCycle {
 
     /**
--- clever-canal-server/src/test/java/org/clever/canal/server/Test01.java
+++ clever-canal-server/src/test/java/org/clever/canal/server/Test01.java
@@ -1,27 +1,24 @@
-package org.clever.canal.node;
+package org.clever.canal.server;
 
-import com.alibaba.otter.canal.filter.aviater.AviaterRegexFilter;
-import com.alibaba.otter.canal.instance.manager.CanalConfigClient;
-import com.alibaba.otter.canal.instance.manager.CanalInstanceWithManager;
-import com.alibaba.otter.canal.instance.manager.ManagerCanalInstanceGenerator;
-import com.alibaba.otter.canal.instance.manager.model.Canal;
-import com.alibaba.otter.canal.instance.manager.model.CanalParameter;
-import com.alibaba.otter.canal.parse.inbound.mysql.MysqlConnection;
-import com.alibaba.otter.canal.parse.inbound.mysql.MysqlEventParser;
-import com.alibaba.otter.canal.parse.inbound.mysql.dbsync.LogEventConvert;
-import com.alibaba.otter.canal.parse.inbound.mysql.dbsync.TableMetaCache;
-import com.alibaba.otter.canal.parse.inbound.mysql.tsdb.DatabaseTableMeta;
-import com.alibaba.otter.canal.parse.inbound.mysql.tsdb.dao.MetaHistoryDAO;
-import com.alibaba.otter.canal.parse.inbound.mysql.tsdb.dao.MetaSnapshotDAO;
-import com.alibaba.otter.canal.protocol.CanalEntry;
-import com.alibaba.otter.canal.protocol.ClientIdentity;
-import com.alibaba.otter.canal.protocol.Message;
-import com.alibaba.otter.canal.protocol.position.EntryPosition;
-import com.alibaba.otter.canal.server.embedded.CanalServerWithEmbedded;
 import com.google.protobuf.ByteString;
 import com.google.protobuf.InvalidProtocolBufferException;
-import com.taobao.tddl.dbsync.binlog.LogEvent;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.RandomUtils;
+import org.clever.canal.filter.aviater.AviaterRegexFilter;
+import org.clever.canal.instance.manager.CanalConfigClient;
+import org.clever.canal.instance.manager.ManagerCanalInstanceGenerator;
+import org.clever.canal.instance.manager.model.Canal;
+import org.clever.canal.instance.manager.model.CanalParameter;
+import org.clever.canal.parse.dbsync.binlog.LogEvent;
+import org.clever.canal.parse.inbound.mysql.MysqlConnection;
+import org.clever.canal.parse.inbound.mysql.dbsync.LogEventConvert;
+import org.clever.canal.parse.inbound.mysql.dbsync.TableMetaCache;
+import org.clever.canal.parse.inbound.mysql.tsdb.MemoryTableMeta;
+import org.clever.canal.protocol.CanalEntry;
+import org.clever.canal.protocol.ClientIdentity;
+import org.clever.canal.protocol.Message;
+import org.clever.canal.protocol.position.EntryPosition;
+import org.clever.canal.server.embedded.CanalServerWithEmbedded;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -33,48 +30,14 @@ import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * 作者：lizw <br/>
- * 创建时间：2019/10/25 16:39 <br/>
+ * 创建时间：2019/10/29 15:10 <br/>
  */
 @Slf4j
-public class CanalTest {
+public class Test01 {
 
     @Test
-    public void t0() throws InterruptedException {
-        Canal canal = new Canal();
-        canal.setId(1L);
-        canal.setName("test");
-        canal.setDesc("test");
-        // canal.setStatus(CanalStatus.START);
-        canal.setGmtCreate(new Date());
-        canal.setGmtModified(new Date());
-        CanalParameter canalParameter = new CanalParameter();
-        canal.setCanalParameter(canalParameter);
-        canalParameter.setCanalId(1L);
-        canalParameter.setDbAddresses(new ArrayList<InetSocketAddress>() {{
-            add(new InetSocketAddress("127.0.0.1", 3306));
-        }});
-        canalParameter.setDbUsername("canal");
-        canalParameter.setDbPassword("canal");
-        canalParameter.setSlaveId(123L);
-        canalParameter.setIndexMode(CanalParameter.IndexMode.MEMORY);
-
-        CanalInstanceWithManager canalInstanceWithManager = new CanalInstanceWithManager(canal, ".*");
-        canalInstanceWithManager.start();
-
-        log.info("### {}", "123");
-        Thread.sleep(1000 * 1000);
-        log.info("### end");
-
-        canalInstanceWithManager.stop();
-    }
-
-    @Test
-    public void t1() throws InterruptedException {
-        MysqlEventParser mysqlEventParser = new MysqlEventParser();
-//        EventTransactionBuffer.TransactionFlushCallback transactionFlushCallback = new
-        log.info("### {}", "123");
-        Thread.sleep(1000 * 1000);
-        log.info("### end");
+    public void t1() {
+        log.info("######## {}", RandomUtils.nextInt(0, 10000));
     }
 
     @Test
@@ -112,7 +75,7 @@ public class CanalTest {
         managerCanalInstanceGenerator.setCanalConfigClient(canalConfigClient);
         CanalServerWithEmbedded canalServerWithEmbedded = CanalServerWithEmbedded.instance();
         canalServerWithEmbedded.setCanalInstanceGenerator(managerCanalInstanceGenerator);
-        canalServerWithEmbedded.setMetricsPort(13306);
+//        canalServerWithEmbedded.setMetricsPort(13306);
         canalServerWithEmbedded.start();
         canalServerWithEmbedded.start("test");
 
@@ -128,11 +91,15 @@ public class CanalTest {
                     Thread.sleep(1000 * 2);
                 } catch (InterruptedException ignored) {
                 }
-                Message message = canalServerWithEmbedded.getWithoutAck(clientIdentity, 1);
+                Message message = canalServerWithEmbedded.get(clientIdentity, 1);
                 if (message.getRawEntries() == null || message.getRawEntries().size() <= 0) {
                     continue;
                 }
                 for (ByteString rawEntry : message.getRawEntries()) {
+                    if (rawEntry == null) {
+                        log.info("rawEntry = {}", rawEntry);
+                        continue;
+                    }
                     try {
                         CanalEntry.Entry entry = CanalEntry.Entry.parseFrom(rawEntry);
                         CanalEntry.RowChange rowChange = CanalEntry.RowChange.parseFrom(entry.getStoreValue());
@@ -161,9 +128,8 @@ public class CanalTest {
         canalServerWithEmbedded.stop();
     }
 
-
     @Test
-    public void t3() throws IOException, InterruptedException {
+    public void t3() throws IOException {
         // 1.构造Mysql连接
         InetSocketAddress address = new InetSocketAddress("127.0.0.1", 3306);
         MysqlConnection connection = new MysqlConnection(address, "canal", "canal", (byte) 33, null);
@@ -200,14 +166,14 @@ public class CanalTest {
         // 缓存表结构
         MysqlConnection metaConnection = connection.fork();
         metaConnection.connect();
-        DatabaseTableMeta tableMetaTSDB = new DatabaseTableMeta();
-        tableMetaTSDB.setConnection(metaConnection);
-        tableMetaTSDB.setFilter(null);
-        tableMetaTSDB.setBlackFilter(null);
+        MemoryTableMeta tableMetaTSDB = new MemoryTableMeta();
+//        tableMetaTSDB.setConnection(metaConnection);
+//        tableMetaTSDB.setMetaHistoryDAO(new MetaHistoryDAO());
+//        tableMetaTSDB.setMetaSnapshotDAO(new MetaSnapshotDAO());
+//        tableMetaTSDB.setFilter(null);
+//        tableMetaTSDB.setBlackFilter(null);
         tableMetaTSDB.init("test");
         TableMetaCache tableMetaCache = new TableMetaCache(metaConnection, tableMetaTSDB);
-        tableMetaTSDB.setMetaHistoryDAO(new MetaHistoryDAO());
-        tableMetaTSDB.setMetaSnapshotDAO(new MetaSnapshotDAO());
 
         // 解析binlog
         LogEventConvert convert = new LogEventConvert();
@@ -255,7 +221,6 @@ public class CanalTest {
             log.info("### =============================================================================================");
             return true;
         });
-
         // Thread.sleep(1000 * 1000);
         log.info("### end");
         connection.disconnect();
