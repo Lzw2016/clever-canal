2e8539cf0891c8968c5f7ac08cd3bfc11b1062ae
lizw <lzw1000000@163.com>
2019-10-31 20:01:10 +0800

    clever-canal-filter 源码分析 - 增加注释

--- clever-canal-filter/src/main/java/org/clever/canal/filter/CanalEventFilter.java
+++ clever-canal-filter/src/main/java/org/clever/canal/filter/CanalEventFilter.java
@@ -7,5 +7,10 @@ import org.clever.canal.filter.exception.CanalFilterException;
  */
 public interface CanalEventFilter<T> {
 
+    /**
+     * 匹配字符串event
+     *
+     * @param event 需要过滤的对象，数据库表名称使用 ${schema}.${tableName} 匹配
+     */
     boolean filter(T event) throws CanalFilterException;
 }
--- clever-canal-filter/src/main/java/org/clever/canal/filter/aviater/AviaterRegexFilter.java
+++ clever-canal-filter/src/main/java/org/clever/canal/filter/aviater/AviaterRegexFilter.java
@@ -9,25 +9,56 @@ import org.clever.canal.filter.exception.CanalFilterException;
 import java.util.*;
 
 /**
- * 基于aviater进行tableName正则匹配的过滤算法
+ * 基于aviater进行tableName正则匹配的过滤算法 <br/>
+ * 使用Perl正则表达式进行匹配，支持多个正则表达式，多个正则表达式使用逗号(,)分隔
+ * <pre>
+ *  1. 所有表： ".*" 或者 ".*\\..*"
+ *  2. canal schema下canal打头的表："canal\\.canal.*"
+ *  3. canal schema下test表："canal\\.test"
+ *  4. 多个规则："canal\\..*,mysql.test1,mysql.test2,"
+ * </pre>
  */
-@SuppressWarnings({"WeakerAccess", "unused"})
+@SuppressWarnings({"WeakerAccess"})
 public class AviaterRegexFilter implements CanalEventFilter<String> {
 
+    /**
+     * 我们的配置的binlog过滤规则可以由多个正则表达式组成，使用逗号”,"进行分割
+     */
     private static final String SPLIT = ",";
+    /**
+     * 将经过逗号",”分割后的过滤规则重新使用|串联起来
+     */
     private static final String PATTERN_SPLIT = "|";
+    /**
+     * canal定义的Aviator过滤表达式，使用了regex自定义函数，接受pattern和target两个参数
+     */
     private static final String FILTER_EXPRESSION = "regex(pattern,target)";
+    /**
+     * regex自定义函数实现，RegexFunction的getName方法返回regex，call方法接受两个参数
+     */
     private static final RegexFunction regexFunction = new RegexFunction();
-    private final Expression exp = AviatorEvaluator.compile(FILTER_EXPRESSION, true);
+    /**
+     * 用于比较两个字符串的大小
+     */
+    private static final Comparator<String> COMPARATOR = new StringComparator();
 
     static {
+        // 将自定义函数添加到AviatorEvaluator中
         AviatorEvaluator.addFunction(regexFunction);
     }
 
-    private static final Comparator<String> COMPARATOR = new StringComparator();
-
-    final private String pattern;
-    final private boolean defaultEmptyValue;
+    /**
+     * 对自定义表达式进行编译，得到Expression对象
+     */
+    private final Expression exp = AviatorEvaluator.compile(FILTER_EXPRESSION, true);
+    /**
+     * 用户设置的过滤规则，需要使用SPLIT进行分割
+     */
+    private final String pattern;
+    /**
+     * 在没有指定过滤规则pattern情况下的默认值，例如默认为true，表示用户不指定过滤规则情况下，总是返回所有的binlog event
+     */
+    private final boolean defaultEmptyValue;
 
     public AviaterRegexFilter(String pattern) {
         this(pattern, true);
@@ -42,7 +73,6 @@ public class AviaterRegexFilter implements CanalEventFilter<String> {
             String[] ss = StringUtils.split(pattern, SPLIT);
             list = Arrays.asList(ss);
         }
-
         // 对pattern按照从长到短的排序
         // 因为 foo|foot 匹配 foot 会出错，原因是 foot 匹配了 foo 之后，会返回 foo，但是 foo 的长度和 foot
         // 的长度不一样
@@ -56,11 +86,9 @@ public class AviaterRegexFilter implements CanalEventFilter<String> {
         if (StringUtils.isEmpty(pattern)) {
             return defaultEmptyValue;
         }
-
         if (StringUtils.isEmpty(filtered)) {
             return defaultEmptyValue;
         }
-
         Map<String, Object> env = new HashMap<>();
         env.put("pattern", pattern);
         env.put("target", filtered.toLowerCase());
