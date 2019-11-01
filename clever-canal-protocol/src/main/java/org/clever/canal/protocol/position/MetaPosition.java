b527519957c19bcd614ea9f6799bc63bd65bdd12
lizw <lzw1000000@163.com>
2019-11-01 12:15:59 +0800

    小调整

--- clever-canal-protocol/src/main/java/org/clever/canal/protocol/position/MetaPosition.java
+++ clever-canal-protocol/src/main/java/org/clever/canal/protocol/position/MetaPosition.java
@@ -1,14 +1,14 @@
 package org.clever.canal.protocol.position;
 
 @SuppressWarnings("unused")
-public class MetaqPosition extends Position {
+public class MetaPosition extends Position {
     private static final long serialVersionUID = -8673508769040569273L;
 
     private String topic;
     private String msgNewId;
     private long offset;
 
-    public MetaqPosition(String topic, String msgNewId, long offset) {
+    public MetaPosition(String topic, String msgNewId, long offset) {
         super();
         this.topic = topic;
         this.msgNewId = msgNewId;
